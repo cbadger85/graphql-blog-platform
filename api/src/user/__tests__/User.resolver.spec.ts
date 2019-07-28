3c67f1131ce3c7220469ed45c1a8d93cfedf7331
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-07-28 16:19:27 -0500

    added test for UserResolver

---
+++ api/src/user/__tests__/User.resolver.spec.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import { UserResolver } from '../User.resolver';
+
+const input = {
+  username: 'username',
+  email: 'email@email.com',
+  password: 'password',
+  name: 'name',
+};
+
+const createdUser = {
+  username: 'username',
+  email: 'email@email.com',
+  password: 'hashedPassword',
+  name: 'name',
+  id: '1',
+};
+
+const userProvider = {
+  createUser: jest.fn(() => createdUser),
+};
+
+const context = {
+  injector: {
+    get: jest.fn(() => userProvider),
+  },
+};
+
+describe('UserResolver', () => {
+  it('should call createUser', async () => {
+    const newUser = UserResolver.Mutation.register(
+      undefined,
+      { input },
+      context as any
+    );
+    expect(userProvider.createUser).toBeCalledWith(input);
+    expect(newUser).toBe(createdUser);
+  });
+});
