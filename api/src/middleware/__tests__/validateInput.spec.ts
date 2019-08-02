956b149c7fb4bd13089d7d3acb1db9f647aa0f53
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2019-08-02 08:13:16 -0500

    updated validationInput.spec.ts

--- api/src/middleware/__tests__/validateInput.spec.ts
+++ api/src/middleware/__tests__/validateInput.spec.ts
@@ -1,5 +1,6 @@
 import { validateInput } from '../validateInput';
 import { createUserValidationSchema } from '../validation/createUserValidation';
+import { Role } from '../../user/types/Role';
 
 let input: any;
 
@@ -17,6 +18,7 @@ describe('validateInput - createUser Mutation', () => {
       password: 'validpassword',
       email: 'validemail@email.com',
       username: 'validUsername',
+      role: [Role.WRITER],
     };
 
     middleware(undefined, { input }, undefined as any, undefined);
@@ -24,12 +26,89 @@ describe('validateInput - createUser Mutation', () => {
     expect(next).toBeCalledWith(undefined, { input }, undefined, undefined);
   });
 
-  it('should throw an error if the input is not valid', async () => {
+  it('should throw an error if the name is not valid', async () => {
     input = {
       name: '',
       password: 'validpassword',
       email: 'validemailemail.com',
       username: 'validUsername',
+      role: [Role.WRITER],
+    };
+
+    const error = await middleware(
+      undefined,
+      { input },
+      undefined as any,
+      undefined
+    ).catch(err => err);
+
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  it('should throw an error if the password is not valid', async () => {
+    input = {
+      name: 'name',
+      password: 123,
+      email: 'validemailemail.com',
+      username: 'validUsername',
+      role: [Role.WRITER],
+    };
+
+    const error = await middleware(
+      undefined,
+      { input },
+      undefined as any,
+      undefined
+    ).catch(err => err);
+
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  it('should throw an error if the email is not valid', async () => {
+    input = {
+      name: 'name',
+      password: 'validPassword',
+      email: 'notAnEmail',
+      username: 'validUsername',
+      role: [Role.WRITER],
+    };
+
+    const error = await middleware(
+      undefined,
+      { input },
+      undefined as any,
+      undefined
+    ).catch(err => err);
+
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  it('should throw an error if the username is not valid', async () => {
+    input = {
+      name: 'name',
+      password: 'validPassword',
+      email: 'notAnEmail',
+      username: '',
+      role: [Role.WRITER],
+    };
+
+    const error = await middleware(
+      undefined,
+      { input },
+      undefined as any,
+      undefined
+    ).catch(err => err);
+
+    expect(error).toBeInstanceOf(Error);
+  });
+
+  it('should throw an error if the role is not valid', async () => {
+    input = {
+      name: 'name',
+      password: 'validPassword',
+      email: 'notAnEmail',
+      username: 'validUsername',
+      role: ['SUPER'],
     };
 
     const error = await middleware(
